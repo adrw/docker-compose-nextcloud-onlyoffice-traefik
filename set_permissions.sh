f1f0e21840aba3dbc13bb8f950ceea36d214d87d
Andrew Alexander <gh@adrw.xyz>
2020-01-16 22:18:38 -0500

    DS edits

--- set_permissions.sh
+++ set_permissions.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+docker exec nextcloud_app apk add --no-cache sudo shadow
+
 # Configure Nextcloud Permissions
 OLDUID=$(docker exec nextcloud_app id -u www-data)
 OLDGID=$(docker exec nextcloud_app id -g www-data)
@@ -12,9 +14,13 @@ docker exec nextcloud_app groupmod -g $NEWGID www-data
 docker exec nextcloud_app find / -user $OLDUID -exec chown -h $NEWUID {} \;
 docker exec nextcloud_app find / -group $OLDGID -exec chgrp -h $NEWGID {} \;
 docker exec nextcloud_app usermod -g $NEWGID www-data
+
 # From Nextcloud docs
-# docker exec nextcloud_app chown -R www-data:www-data /var/html
-# docker exec nextcloud_app chmod -R 0750 /var/html
+docker exec nextcloud_app chown -R www-data:users /var/html
+docker exec nextcloud_app chmod -R 0750 /var/html
+
+docker exec nextcloud_app chown -R www-data:users /data
+docker exec nextcloud_app chmod -R 0750 /data
 
 UPDATED_UID=$(docker exec nextcloud_app id -u www-data)
 UPDATED_GID=$(docker exec nextcloud_app id -g www-data)
