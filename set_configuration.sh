d8a6e98a6ef7a83f1ce8b9be1de31c36d999e6c2
Andrew Alexander <adrw@squareup.com>
2020-01-17 01:55:09 -0500

    Update docker exec user for linuxserver

--- set_configuration.sh
+++ set_configuration.sh
@@ -3,13 +3,13 @@
 set -x
 
 # Configure Nextcloud
-docker exec -u www-data nextcloud_app php occ --no-warnings config:system:set trusted_domains 0 --value="nextcloud_nginx"
-docker exec -u www-data nextcloud_app php occ --no-warnings config:system:set trusted_domains 1 --value="${LOCAL:-192.168.0.116:40480}" # local address
-docker exec -u www-data nextcloud_app php occ --no-warnings config:system:set trusted_domains 2 --value="${DOMAIN:-cloud.adrw.xyz}" # remote address
-echo Trusted Domains $(docker exec -u www-data nextcloud_app php occ --no-warnings config:system:get trusted_domains)
+docker exec -u abc nextcloud_app php occ --no-warnings config:system:set trusted_domains 0 --value="nextcloud_nginx"
+docker exec -u abc nextcloud_app php occ --no-warnings config:system:set trusted_domains 1 --value="${LOCAL:-192.168.0.116:40480}" # local address
+docker exec -u abc nextcloud_app php occ --no-warnings config:system:set trusted_domains 2 --value="${DOMAIN:-cloud.adrw.xyz}" # remote address
+echo Trusted Domains $(docker exec -u abc nextcloud_app php occ --no-warnings config:system:get trusted_domains)
 
-docker exec -u www-data nextcloud_app php occ --no-warnings app:install onlyoffice
+docker exec -u abc nextcloud_app php occ --no-warnings app:install onlyoffice
 
-docker exec -u www-data nextcloud_app php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
-docker exec -u www-data nextcloud_app php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://nextcloud_onlyoffice/"
-docker exec -u www-data nextcloud_app php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nextcloud_nginx/"
+docker exec -u abc nextcloud_app php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
+docker exec -u abc nextcloud_app php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://nextcloud_onlyoffice/"
+docker exec -u abc nextcloud_app php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nextcloud_nginx/"
